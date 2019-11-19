1a47ad45267f25fc57597bc7cf54f9a2441b6990
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-11-19 03:29:46 -0500

    created show for farmers markets and conditional logic to render

--- frontend/eat-fresh-frontend/src/App.js
+++ frontend/eat-fresh-frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Home from "./components/Home/Home";
 import ShowPage from "./components/ShowPage/ShowPage";
 import Axios from 'axios';
+
+
+
+
 class App extends Component {
   constructor(state) {
     super(state);
@@ -14,70 +18,92 @@ class App extends Component {
       produce:"",
       produceInSeason: null,
       localMarkets: [{"id": "Error"}],
-      zip: ""
+      zip: "",
+      markets: ""
     };
   }
-
- componentDidMount() {
-   Axios.get('http://localhost:8000/produces')
-   .then(res => {
-     console.log(res.data);
-    this.setState({produce: res.data});
-   });
- };
-
- componentDidUpdate(prevProps, prevState){
-  console.log(prevState)
-  console.log(this.state)
-  if(prevState.region !== this.state.region || prevState.season !==  this.state.season){
-     this.setState({produceInSeason: null})
-    if(this.state.region !== "" && this.state.season !== ""){
-      let inSeason =[];
-      for(let i=0; i < this.state.produce.length; i++){
+  
+  componentDidMount() {
+    Axios.get('http://localhost:8000/produces')
+    .then(res => {
+      console.log(res.data);
+      this.setState({produce: res.data});
+    });
+  };
+  
+  componentDidUpdate(prevProps, prevState){
+    console.log(prevState)
+    console.log(this.state)
+    if(prevState.region !== this.state.region || prevState.season !==  this.state.season){
+      this.setState({produceInSeason: null})
+      if(this.state.region !== "" && this.state.season !== ""){
+        let inSeason =[];
+        for(let i=0; i < this.state.produce.length; i++){
           for(let j=0; j < this.state.produce[i].seasonAndRegion[this.state.season].length; j++){
-          //console.log(this.state.produce[i].seasonAndRegion[this.state.season])
-              if(this.state.produce[i].seasonAndRegion[this.state.season][j] === this.state.region){
-                inSeason.push(this.state.produce[i])
-                console.log(inSeason)
-              }
+            //console.log(this.state.produce[i].seasonAndRegion[this.state.season])
+            if(this.state.produce[i].seasonAndRegion[this.state.season][j] === this.state.region){
+              inSeason.push(this.state.produce[i])
+              console.log(inSeason)
+            }
           }
-      }
-      if(inSeason.length > 0){
-      this.setState({produceInSeason: inSeason})
+        }
+        if(inSeason.length > 0){
+          this.setState({produceInSeason: inSeason})
+        }
       }
     }
-  }
+    
 
-  if(this.state.localMarkets[0].id === "Error"){
-    console.log('hello')
+  //   if(this.state.localMarkets[0].id !== "Error"){
+  //    let showMarkets = this.state.localMarkets.map(item => {
+  //      return (
+  //          <div>
+  //              <h4>{item.marketname}</h4>
+  //          </div>
+  //      )
+  //  })
+  //  this.setState({markets: showMarkets})
+  //  }
   }
- }
-
- setRegion = event => {
-   //console.log(event.target.innerHTML);
+    
+    
+  setRegion = event => {
+      //console.log(event.target.innerHTML);
    this.setState({region: event.target.name})
  
- }
+  }
 
- setSeason = event => {
+  setSeason = event => {
    this.setState({season: event.target.innerHTML})
- }
+  }
 
- setZip = event => {
+  setZip = event => {
    console.log(event.target.value)
    this.setState({zip: event.target.value})
 
- }
+  }
 
- findMarket = () => {
+  findMarket = () => {
    Axios.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip="+ this.state.zip)
    .then(res => {
      console.log(res.data.results)
      this.setState({localMarkets: res.data.results})
    })
+   
  }
 
   render() {
+    let showMarkets = null
+    if(this.state.localMarkets[0].id !== "Error"){
+        showMarkets = this.state.localMarkets.map(item => {
+        return (
+            <div>
+                <h4>{item.marketname}</h4>
+            </div>
+        )
+    })
+    }
+
     return (
       <div>
         <nav>
@@ -124,8 +150,19 @@ class App extends Component {
           </Switch>
         </main>
         <div>
-          <h3>Find a farmer's market near you!</h3>
+          <h3>Find a farmer's market near you?</h3>
           <input onChange={this.setZip} type="text" placeholder="enter a zipcode"/> <button onClick={this.findMarket}>enter</button>
+
+          {this.state.localMarkets[0].id !== "Error" ? (
+          <div>
+            <h3>{showMarkets}</h3>
+          </div>
+
+          ):(
+            <div>
+              
+            </div>
+          )}
         </div>
       </div>
     )
--- frontend/eat-fresh-frontend/src/components/Showpage/Showpage.js
+++ frontend/eat-fresh-frontend/src/components/Showpage/Showpage.js
@@ -6,20 +6,22 @@ class ShowPage extends Component {
   }
     
   render() {
-      let showMarkets = this.props.localMarkets.map(item => {
-          return (
-              <div>
-                  <h4>{item.marketname}</h4>
-              </div>
-          )
-      })
+    //   let showMarkets = this.props.localMarkets.map(item => {
+    //       return (
+    //           <div>
+    //               <h4>{item.marketname}</h4>
+    //           </div>
+    //       )
+    //   })
       return (
           <div>
               <h2>Markets Near You!</h2>
 
-              <div>{showMarkets}</div>
+              {/* <div>{showMarkets}</div> */}
           </div>
       )
   }
 }
+
+    
 export default ShowPage;
