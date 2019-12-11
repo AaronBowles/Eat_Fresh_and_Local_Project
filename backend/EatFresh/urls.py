713199268beb82609c2790bab225f620ce081a73
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-12-11 10:29:32 -0500

    commit for update

--- backend/EatFresh/urls.py
+++ backend/EatFresh/urls.py
@@ -3,6 +3,7 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
+    path('', views.ProduceList.as_view(), name='produce_list'),
     path('produces', views.ProduceList.as_view(), name='produce_list'),
     path('produces/<int:pk>', views.ProduceDetail.as_view(), name='produce_detail'),
 ]
--- backend/EatFresh_django/settings.py
+++ backend/EatFresh_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o253+^w=wo2n#p-0r^7qrqu!*^t+mdj%sred!hvzilw#mwr#%6'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eatfreshandlocalapp.herokuapp.com']
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -49,7 +50,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -85,15 +85,9 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'eatfresh',
-        'USER': 'eatfreshuser',
-        'PASSWORD': 'eatfresh',
-        'HOST': 'localhost'
-    }
-}
+# DATABASES = {
+# }
+DATABASES = { 'default' : dj_database_url.config()}
 
 
 # Password validation
@@ -144,4 +138,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
