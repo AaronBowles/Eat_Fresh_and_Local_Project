76720ec974f9581c22fd08322375556f0f870094
Aaron <Bowles@Aarons-MacBook-Pro.local>
2020-02-04 12:30:07 -0500

    minor css change

--- frontend/eat-fresh-frontend/src/components/Home/Home.js
+++ frontend/eat-fresh-frontend/src/components/Home/Home.js
@@ -42,32 +42,7 @@ class Home extends Component {
         //this.setState({produceSelected: null})
     }
   render() {
-    // if(this.props.region !== "" && this.props.season !== ""){
-    //     for(let i=0; i < this.props.produce.length; i++){
-    //         for(let j=0; j < this.props.produce[i].seasonAndRegion[this.props.season].length; j++){
-    //         //console.log(this.props.produce[i].seasonAndRegion[this.props.season])
-    //             if(this.props.produce[i].seasonAndRegion[this.props.season][j] === this.props.region){
-    //                 this.props.produceInSeason.push(this.props.produce[i])
-    //             }
-    //         }
-    //     }
-    // }
 
-    // let inSeasonList = null;
-    
-    // if(this.props.produceInSeason !== null){
-        //      inSeasonList = this.props.produceInSeason.map(item => {
-            //         console.log(item)
-            //         return (
-                //             <div className="produceDetail">
-                //                 <h3>Name: {item.name}</h3>
-                //                 <h4>Category: {item.category}</h4>
-                //                 <h4>Description:</h4><p>{item.description}</p>
-                //             </div>
-                //         )
-                //     })
-                
-                // }
     let produceDetail = null
     if(this.state.produceSelected !== null && this.props.produceInSeason !== null){
         // for(let i=0; i< this.props.produceInSeason.length; i++){
